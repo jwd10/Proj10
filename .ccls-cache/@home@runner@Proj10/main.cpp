78c9540f0748524ce1662840f785e6883127d1c2
jwd10 <129916407+jwd10@users.noreply.github.com>
2023-04-05 03:00:53 +0000

    Part 1 implemented

--- .ccls-cache/@home@runner@Proj10/main.cpp
+++ .ccls-cache/@home@runner@Proj10/main.cpp
@@ -1,5 +1,44 @@
+/*
+* Author: John Devine
+* Course: 16365 COP2334 202301
+* Editor: Replit
+* Description: Takes input from user, uses it to calculate and output operations. Reports the number of loops and exits after -1 is entered
+*/
+
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  //VARIABLES
+  int counter = 0; //loop counter
+  double input; //stores user input
+
+  //main loop
+  while (true) {
+    cout << "Please enter a number(-1 to exit):";
+
+    //input check loop, program loops endlessly on invalid input without it 
+    while (!(cin >> input)) {
+			cout << "Error, please enter a number" << endl;
+			
+			cin.clear();
+			
+			cin.ignore(256, '\n');			
+		}
+
+    //exit if input is -1
+    if(static_cast<int>(input) == -1) break;
+
+    //output operations
+    cout << input * input << endl;
+    cout << input / input << endl; //outputs -nan if input is 0
+    cout << (input + 3.0) / 5.0 << endl;
+    cout << ((input + 3.0) / 5.0) + ((input + 7.0) / 2.0) << endl;
+    ++counter; //increment counter
+  }
+
+  //output counter
+  cout << counter << " loops" << endl;
+
+  return 0;
 }
